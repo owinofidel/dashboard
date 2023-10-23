48d706535541179853c16c2a74683a4a26b5e7ec
Fidel Owino <78029452+owinofidel@users.noreply.github.com>
2023-10-23 17:19:40 +0300

    Add files via upload
    This dashboard is coming along nicely

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-const link = document.querySelectorAll(".links");
-console.log(link)
+const resizeBtn = document.querySelector(".resize-container");
+const navElement = document.querySelector(".container");
+const linksText = document.querySelectorAll(".link-text")
+let iconState = true;
+
+resizeBtn.addEventListener("click", () => {
+    navElement.classList.toggle("minimize")
+    resizeBtn.classList.toggle("resize-container-sm-container")
+    addIconText(iconState)
+    iconState = !iconState;
+})
+
+const addIconText = (value) => {
+    if(value){
+        setTimeout(()=>{
+            for(let i = 0 ; i < linksText.length; i++){
+                    // linksText[i].classList.toggle("full"
+                    linksText[i].classList.toggle("visible")
+                }
+        }, 100)
+        
+    } else {
+        setTimeout(()=>{
+            for(let i = 0 ; i < linksText.length; i++){
+                    // linksText[i].classList.toggle("full")
+                    linksText[i].classList.toggle("visible")
+                }
+        }, 400)
+    }
+}
